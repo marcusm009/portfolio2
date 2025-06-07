7ab4c05759b6217609181332ea75a0930ab4c68f
Marcus Mills <marcusm009@gmail.com>
2025-06-07 11:13:58 -0600

    Initial boilerplate code

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { App } from './App';
+
+console.log(`main.ts starting ${App.name}`);
+window.addEventListener('DOMContentLoaded', () => {
+    let canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
+    let app = new App(canvas);
+    app.run();
+});
